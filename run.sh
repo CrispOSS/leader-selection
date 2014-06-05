c891e313d75ba361f385a549a9a11509e16ddb81
Behrooz Nobakht <behrooz.nobakht@gmail.com>
2014-06-05 11:45:58 +0200

    Use ABS API from Maven Central

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-javac -cp .:/home/vlad/abs-api-git/abs-api-remote/target/abs-api-remote-1.0-SNAPSHOT.jar:/home/vlad/abs-api-git/abs-api/target/abs-api-1.0-SNAPSHOT.jar *.java -d ../bin
+#!/bin/bash
+
+mvn exec:java -Dexec.mainClass=com.github.crisposs.leaderselection.Main -Dexec.args="2"
+
